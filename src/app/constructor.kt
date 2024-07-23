3f86e66e531412ebdcf8eb6cd79d0dceb2534977
Alfat <alfatnurhidayatrevisi@gmail.com>
2024-07-24 01:12:45 +0700

    Initializer Block

--- src/app/constructor.kt
+++ src/app/constructor.kt
@@ -5,4 +5,7 @@ import data.Cars
 fun main() {
     var toyota = Cars("Mazda", "Rotary")
     println(toyota.year)
+
+    var porsche = Cars("GT-2", "Porsche", 2023)
+    println(porsche.nameBrand)
 }
--- src/data/Cars.kt
+++ src/data/Cars.kt
@@ -1,6 +1,9 @@
 package data
 
 class Cars(brand: String, machine: String, year:Int = 2020 ) {
+    init {
+        println("Congratulations your new cars is  ${brand}")
+    }
     var nameBrand: String = brand
     var machineCar: String = machine
     var year:Int = year
