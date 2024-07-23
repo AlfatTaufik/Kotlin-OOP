e44cb2bf767e971d12ca7e58bd7e4995078ec0e5
Alfat <alfatnurhidayatrevisi@gmail.com>
2024-07-24 01:07:39 +0700

    constructor

---
+++ src/app/constructor.kt
@@ -0,0 +1,8 @@
+package app
+
+import data.Cars
+
+fun main() {
+    var toyota = Cars("Mazda", "Rotary")
+    println(toyota.year)
+}
---
+++ src/data/Cars.kt
@@ -0,0 +1,7 @@
+package data
+
+class Cars(brand: String, machine: String, year:Int = 2020 ) {
+    var nameBrand: String = brand
+    var machineCar: String = machine
+    var year:Int = year
+}
