e93dec3c0fb967ad04aa396c7b5b7db9d6f3e753
Alfat <alfat601@smk.belajar.id>
2024-07-25 02:30:39 +0700

    Overriding

--- src/data/Employee.kt
+++ src/data/Employee.kt
@@ -1,12 +1,18 @@
 package data
 
 open class Employee(var name: String) {
-    fun sayBello(fullname:String){
+    open fun sayBello(fullname:String){
         println("Bello ${name}'s say ${fullname}")
     }
 }
 
 class Employee1(name: String) : Employee(name){
-
+    override fun sayBello(fullname: String) {
+        println("Hello my name is $fullname iam employee 1")
+    }
 }
-class ViceMentor(name: String) : Employee(name){}
+class ViceMentor(name: String) : Employee(name){
+    override fun sayBello(fullname: String) {
+        println("Vice me name is $fullname iam vice president")
+    }
+}
