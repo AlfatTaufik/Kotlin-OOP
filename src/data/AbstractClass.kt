a5755a32015a3e4661ac2dec874715b732d20caf
Alfat <alfat601@smk.belajar.id>
2024-08-05 23:09:22 +0700

    abstract prop dan function

--- src/data/AbstractClass.kt
+++ src/data/AbstractClass.kt
@@ -1,8 +1,16 @@
 package data
 
 abstract class AbstractClass(var name: String = "Juned") {
+    abstract var what: String
+    abstract fun run(name: String)
 }
 
-class Turunan(name: String) : AbstractClass(name) {}
+class Turunan(name: String) : AbstractClass(name) {
+    override var what: String = "Turunan"
+
+    override fun run(name: String) {
+        println("what is running $what")
+    }
+}
 
 //Kelas abstract gabisa dibuat object, hanya bisa diturunkan
