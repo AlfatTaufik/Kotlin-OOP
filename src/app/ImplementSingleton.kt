e1fa3f176fa41972137280b26bda31a57874e908
Alfat <alfat601@smk.belajar.id>
2024-08-15 21:07:32 +0700

    Singleton Objecg

---
+++ src/app/ImplementSingleton.kt
@@ -0,0 +1,8 @@
+package app
+
+import data.Utilities
+
+fun main() {
+    println(Utilities.name)
+    println(Utilities.toUpper("Nothing"))
+}
---
+++ src/data/SingletoObj.kt
@@ -0,0 +1,8 @@
+package data
+
+object Utilities {
+    var name: String = "Token"
+    fun toUpper(value : String) : String{
+        return value.toUpperCase()
+    }
+}
